c923eb8ad4f51f108cbaad0d07a1fffd9d63a26b
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-10-17 16:51:52 +0530

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Pradyumn Shukla on 17/10/20.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
